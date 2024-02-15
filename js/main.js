9d101be0de4243a13fd7831184d802ca7cb84696
jun <emfowkd@gmail.com>
2024-02-16 00:38:37 +0900

    2024-02-15

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-const slideWrapper = document.querySelector('.slideWrapper');
-const slideContainer = slideWrapper.querySelector('.slideContainer');
-const slides = slideContainer.querySelectorAll('li');
+const slideWrapper = document.querySelector(".slideWrapper");
+const slideContainer = slideWrapper.querySelector(".slideContainer");
+const slides = slideContainer.querySelectorAll("li");
 const slidesCount = slides.length;
 const slideToShow = 3;
 // ------------------------------app
 
-const appSection = document.querySelector('.app')
-const appTextContainer = appSection.querySelector('.textContainer')
-const appTitle = appTextContainer.querySelector('h2')
-const appDesc = appTextContainer.querySelector('h3')
-console.log('test',appTextContainer.offsetTop)
+const appSection = document.querySelector(".app");
+const appTextContainer = appSection.querySelector(".textContainer");
+const appTitle = appTextContainer.querySelector("h2");
+const appDesc = appTextContainer.querySelector("h3");
+console.log("test", appTextContainer.offsetTop);
 
+window.addEventListener("scroll", () => {
+  showToRight(appTitle, scrollY);
+  showToRight(appDesc, scrollY);
+});
 
-
-window.addEventListener('scroll', ()=> {
-  if (scrollY > appTextContainer.offsetTop-800) {
-    appTextContainer.style.transform = 'translateX(0%)'
-    appTextContainer.style.opacity = 1
-    appTextContainer.style.visibility = 'visible'
-  } else if (scrollY < 300) {
-    appTextContainer.style.transform = 'translateX(-100%)'
-    appTextContainer.style.opacity = 0
-    appTextContainer.style.visibility = 'hidden'
+function showToRight(target, scrollMount) {
+  if (scrollMount > target.offsetTop - 600) {
+    target.style.transform = "translateX(0%)";
+    target.style.opacity = 1;
+    target.style.visibility = "visible";
+  } else if (scrollMount < 300) {
+    target.style.transform = "translateX(-100%)";
+    target.style.opacity = 0;
+    target.style.visibility = "hidden";
   }
-})
-
+}
