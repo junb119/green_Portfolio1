95ffe154a9e334249d5a904acba32989c4d98785
jun <emfowkd@gmail.com>
2024-02-20 01:01:48 +0900

    2024-02-19

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
-  // ---------------------------header
-  const header = document.querySelector('header')
-  const headerHeight = header.offsetHeight
-  const gnbTitle = header.querySelectorAll('.gnbTitle')
+// ---------------------------header
+const header = document.querySelector("header");
+const headerHeight = header.offsetHeight;
+const gnbTitle = header.querySelectorAll(".gnbTitle");
 
-  for (let title of gnbTitle) {
-    title.addEventListener('mouseenter', ()=> {
-      header.style.height = title.querySelector('.gnbMenu').offsetHeight + headerHeight + 16 + 'px'
-      // console.log(header.offsetHeight)
-    })
+for (let title of gnbTitle) {
+  title.addEventListener("mouseenter", () => {
+    title.querySelector(".gnbMenu").style.display = "block";
+    console.log(title.querySelector(".gnbMenu").offsetHeight);
 
-    title.addEventListener('mouseleave', ()=> {
-      header.style.height = headerHeight + 'px'
-    })
-  }
+    header.style.height =
+      title.querySelector(".gnbMenu").offsetHeight + headerHeight + 16 + "px";
 
+    // console.log(header.offsetHeight)
+  });
 
+  title.addEventListener("mouseleave", () => {
+    header.style.height = headerHeight + "px";
+    title.querySelector(".gnbMenu").style.display = "none";
+  });
+}
+// ------------------------items
 
-
+const itemsWrapper = document.querySelector(".itemsWrapper");
+const itemsH2 = itemsWrapper.querySelector("h2");
+const itemsH3 = itemsWrapper.querySelector("h3");
+const items = itemsWrapper.querySelector(".items");
 
 // -----------------------slide
 const slideWrapper = document.querySelector(".slideWrapper");
@@ -26,32 +34,20 @@ const slidesCount = slides.length;
 const slideToShow = 3;
 
 function resizeSlide() {
-  let bodyWidth = document.body.offsetWidth
+  let bodyWidth = document.body.offsetWidth;
   for (let slide of slides) {
-    slide.style.width = bodyWidth * 0.3 + 'px'
-    slide.style.marginLeft = bodyWidth * 0.05 + 'px'
+    slide.style.width = bodyWidth * 0.3 + "px";
+    slide.style.marginLeft = bodyWidth * 0.05 + "px";
   }
-  slides[0].style.marginLeft = 0 
-  slideContainer.style.width = bodyWidth * 0.3 * slides.length + 'px'
+  slides[0].style.marginLeft = 0;
+  slideContainer.style.width = bodyWidth * 0.3 * slides.length + "px";
 }
-resizeSlide()
+resizeSlide();
 
-window.addEventListener('resize',()=> {
-  console.log(document.body.offsetWidth * 0.3)
-  resizeSlide()
-
-})
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener("resize", () => {
+  console.log(document.body.offsetWidth * 0.3);
+  resizeSlide();
+});
 
 // ------------------------------app
 
@@ -60,12 +56,27 @@ const appTitle = appTextContainer.querySelector("h2");
 const appDesc = appTextContainer.querySelector("h3");
 
 window.addEventListener("scroll", () => {
-  showToRight(appTitle, scrollY);
-  showToRight(appDesc, scrollY);
+  showToRight(appTextContainer, appTitle, scrollY);
+  showToRight(appTextContainer, appDesc, scrollY);
+  showToUp(itemsWrapper, itemsH2, scrollY);
+  showToUp(itemsWrapper, itemsH3, scrollY);
+  showToUp(itemsWrapper, items, scrollY);
 });
 
-function showToRight(target, scrollMount) {
-  if (scrollMount > appTextContainer.offsetTop - 600) {
+function showToUp(container, target, scrollMount) {
+  if (scrollMount > container.offsetTop - 600) {
+    target.style.transform = "translateY(0%)";
+    target.style.opacity = 1;
+    target.style.visibility = "visible";
+  } else if (scrollMount < 300) {
+    target.style.transform = "translateY(100px)";
+    target.style.opacity = 0;
+    target.style.visibility = "hidden";
+  }
+}
+
+function showToRight(container, target, scrollMount) {
+  if (scrollMount > container.offsetTop - 600) {
     target.style.transform = "translateX(0%)";
     target.style.opacity = 1;
     target.style.visibility = "visible";
