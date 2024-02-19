506b5b5e54fe52d195caea8e1371737bdbe68a2a
junb119 <emfowkd@gmail.com>
2024-02-19 18:23:05 +0900

    header update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
+  // ---------------------------header
   const header = document.querySelector('header')
   const headerHeight = header.offsetHeight
   const gnbTitle = header.querySelectorAll('.gnbTitle')
 
-  console.log('header test')
-
-
   for (let title of gnbTitle) {
     title.addEventListener('mouseenter', ()=> {
       header.style.height = title.querySelector('.gnbMenu').offsetHeight + headerHeight + 16 + 'px'
-      console.log(header.offsetHeight)
+      // console.log(header.offsetHeight)
     })
 
     title.addEventListener('mouseleave', ()=> {
@@ -16,23 +14,33 @@
     })
   }
 
+
+
+
+
+// -----------------------slide
 const slideWrapper = document.querySelector(".slideWrapper");
 const slideContainer = slideWrapper.querySelector(".slideContainer");
 const slides = slideContainer.querySelectorAll("li");
 const slidesCount = slides.length;
 const slideToShow = 3;
 
+function resizeSlide() {
+  let bodyWidth = document.body.offsetWidth
+  for (let slide of slides) {
+    slide.style.width = bodyWidth * 0.3 + 'px'
+    slide.style.marginLeft = bodyWidth * 0.05 + 'px'
+  }
+  slides[0].style.marginLeft = 0 
+  slideContainer.style.width = bodyWidth * 0.3 * slides.length + 'px'
+}
+resizeSlide()
 
+window.addEventListener('resize',()=> {
+  console.log(document.body.offsetWidth * 0.3)
+  resizeSlide()
 
-
-
-
-
-
-
-
-
-
+})
 
 
 
